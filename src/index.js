bec7395d739e28ad9fcd42cfe0fb77c8be9871bb
kendofriendo <goadke@g.cofc.edu>
2021-04-09 11:24:08 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  //grab all the necessary DOM elements
-
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
-
-  //attach event listeners
-  newTaskForm.addEventListener("submit", createNewTask);
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", newTask);
 });
-
-const createNewTask = event => {
+const newTask = event => {
   event.preventDefault();
-  //stop form from trying to submit
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
-
-  appendNewTask(newTask);
-  event.target.reset();
-};
-
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
+  const taskList = document.getElementById("tasks")
+  const taskDesc = document.getElementById("new-task-description")
+  const createNewTask = document.createElement("li");
+  createNewTask.innerText = taskDesc.value;
+  taskList.appendChild(createNewTask)
 };
