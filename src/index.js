d3e8fb8c0b3032c8b4e4f45200e8b1af6e597304
kendofriendo <goadke@g.cofc.edu>
2021-04-09 05:00:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", newTask);
 });
+
+const newTask = event => {
+  event.preventDefault();
+  const taskList = document.getElementById("tasks")
+  const taskDesc = document.getElementById("new-task-description")
+  const createNewTask = document.createElement("li");
+  createNewTask.innerText = taskDesc.value;
+  taskList.appendChild(createNewTask)
+};
